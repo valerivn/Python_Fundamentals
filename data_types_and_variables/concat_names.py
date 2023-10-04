13e29b7700ede8eed72e4603a304e186b29d432b
valerivn <128290008+valerivn@users.noreply.github.com>
2023-10-04 17:26:43 +0300

    Add files via upload

---
+++ data_types_and_variables/concat_names.py
@@ -0,0 +1,9 @@
+first_name = input()
+last_name = input()
+delimiter = input()
+print(first_name + delimiter + last_name)
+
+# first_name = input()
+# second_name = input()
+# symbols = input()
+# print(f"{first_name}{symbols}{second_name}") we can add int outside of {}
